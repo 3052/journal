73eb411ec7055b7bb550fb3bfb65e6e28501f425
27 <27@riseup.net>
2026-05-02 17:39:52 -0500

    deleted:    2026/5/nordVpn/http.go

--- 2026/5/nordVpn/nordVpn.go
+++ 2026/5/nordVpn/nordVpn.go
@@ -1,11 +1,20 @@
-package nordVpn
+package main
 
 import (
    "encoding/json"
+   "errors"
+   "flag"
+   "fmt"
    "io"
+   "log"
+   "net/http"
    "net/url"
+   "os"
+   "os/exec"
+   "path/filepath"
    "strconv"
    "strings"
+   "time"
 )
 
 // limit <= -1 for default
@@ -84,4 +93,122 @@ type Server struct {
          Code string
       }
    }
+}
+func Get(targetUrl *url.URL, headers map[string]string) (*http.Response, error) {
+   reqHeader := make(http.Header)
+   for key, value := range headers {
+      reqHeader.Set(key, value)
+   }
+   req := &http.Request{
+      Method: http.MethodGet,
+      URL:    targetUrl,
+      Header: reqHeader,
+   }
+
+   log.Println(req.Method, req.URL)
+   return http.DefaultClient.Do(req)
+}
+func main() {
+   log.SetFlags(log.Ltime)
+   err := new(client).do()
+   if err != nil {
+      log.Fatal(err)
+   }
+}
+
+func (c *client) do() error {
+   var err error
+   c.cache, err = os.UserCacheDir()
+   if err != nil {
+      return err
+   }
+   c.cache = filepath.Join(c.cache, "nordVpn/nordVpn.json")
+   // 1
+   flag.BoolVar(&c.write, "w", false, "write")
+   // 2
+   flag.StringVar(&c.country_code, "c", "", "country code")
+   flag.Parse()
+   if c.write {
+      return c.do_write()
+   }
+   if c.country_code != "" {
+      return c.do_country_code()
+   }
+   flag.Usage()
+   return nil
+}
+
+func write_file(name string, data []byte) error {
+   log.Println("WriteFile", name)
+   return os.WriteFile(name, data, os.ModePerm)
+}
+
+func (c *client) do_write() error {
+   data, err := WriteServers(0)
+   if err != nil {
+      return err
+   }
+   return write_file(c.cache, data)
+}
+
+type client struct {
+   cache string
+   // 1
+   write bool
+   // 2
+   country_code string
+}
+
+func (c *client) do_country_code() error {
+   data, err := read_file(c.cache)
+   if err != nil {
+      return err
+   }
+   servers, err := ReadServers(data)
+   if err != nil {
+      return err
+   }
+   data, err = exec.Command("credential", "-j=api.nordvpn.com").Output()
+   if err != nil {
+      return err
+   }
+   var credential []struct {
+      Username string
+      Password string
+   }
+   err = json.Unmarshal(data, &credential)
+   if err != nil {
+      return err
+   }
+   for _, server := range servers {
+      if server.ProxySsl() {
+         if server.Country(c.country_code) {
+            fmt.Println(
+               FormatProxy(
+                  credential[0].Username, credential[0].Password,
+                  server.Hostname,
+               ),
+            )
+         }
+      }
+   }
+   return nil
+}
+
+const duration = 24 * time.Hour
+
+func read_file(name string) ([]byte, error) {
+   file, err := os.Open(name)
+   if err != nil {
+      return nil, err
+   }
+   defer file.Close()
+   info, err := file.Stat()
+   if err != nil {
+      return nil, err
+   }
+   if time.Since(info.ModTime()) >= duration {
+      return nil, errors.New(duration.String())
+   }
+   return io.ReadAll(file)
 }
